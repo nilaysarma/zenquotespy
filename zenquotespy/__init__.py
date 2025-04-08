5e3f091f2f9edb02dab4c08d2c8606b2c4a646a2
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-04-08 20:05:37 +0530

    Update v0.1.1

--- zenquotespy/__init__.py
+++ zenquotespy/__init__.py
@@ -1,3 +1,8 @@
+"""ZenquotesPy
+=====
+ZenquotesPy is a lightweight Python package that provides easy access to motivational and inspirational quotes from the ZenQuotes.io API.
+"""
+
 from .main import random
 from .main import today
 from .main import get_bulk_quotes
